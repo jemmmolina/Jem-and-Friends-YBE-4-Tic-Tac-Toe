4589e13a68ceb6453d510bda027168c5bc8c9b5c
Jemimah Mae Molina <Jemimah Mae Molina>
2016-04-06 21:46:29 +0800

    class Player and Main

---
+++ Player.java
@@ -0,0 +1,18 @@
+public class Player {
+	
+	private String name;
+	private int score;
+
+	public String getName() {
+		return this.name;
+	}
+
+	public int getScore() {
+		return this.score;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
