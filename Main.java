3c797c2b36705c36026dc887088a865b799bfaaa
[amsartillo] <[amsartillo@up.edu.ph]>
2016-04-07 13:07:51 +0800

    Modified Main class java

--- Main.java
+++ Main.java
@@ -1,60 +1,72 @@
 import java.util.Scanner;
 
 public class Main {
-/**
-	public class TTTConsole {				//for console run
-	   public static final int EMPTY = 0;
-	   public static final int CROSS = 1;
-	   public static final int NONE = 2;
-	   
-	   public static final int PLAYING = 0;
-	   public static final int DRAW = 1;
-	   public static final int CROSS_WON = 2;
-	   public static final int NONE_WON = 3;
-	 
-	   public static final int ROWS = 3, COLS = 3;
-	   public static int[][] board = new int[ROWS][COLS];
-	   public static int currentState;
+   private Board board;
+   private GameState currentState;
+   private Seed currentPlayer;
+ 
+   private static Scanner in = new Scanner(System.in);
 
-	   public static int currentPlayer;
-	   public static int currntRow, currentCol;
-	 
-	   public static Scanner in = new Scanner(System.in); // the input Scanner
-	}
-**/	
-	public static void main(String[] args) {
-		Board b = new Board();
-		
-		initGame();
-		
-		do {
-			playerMove(currentPlayer);
-			updateGame(currentPlayer, currentRow, currentCol);
-			printBoard();
-		}
-		
-		public static void printBoard() {
-		for (int row = 0; row < ROWS; ++row) {
-			for (int col = 0; col < COLS; ++col) {
-				printCell(board[row][col]); // print each of the cells
-					if (col != COLS - 1) {
-						System.out.print("|");   // print vertical partition
-					}
-			}
-			System.out.println();
-			if (row != ROWS - 1) {
-				System.out.println("-----------"); // print horizontal partition
-			}
-		}
-		System.out.println();
-		}
-		
-		public static void printCell(int content) {
-			switch (content) {
-				case NONE: System.out.print(" O "); break;
-				case CROSS:  System.out.print(" X "); break;
-				case EMPTY:  System.out.print("   "); break;		
-			}
-		}
-	}
+   public GameMain() {
+      board = new Board();
+ 
+      initGame();
+
+      do {
+         playerMove(currentPlayer);
+         board.paint();             // ask the board to paint itself
+         updateGame(currentPlayer);
+
+         if (currentState == GameState.CROSS_WON) {
+            System.out.println("'X' won! Game over!");
+         } else if (currentState == GameState.NONE_WON) {
+            System.out.println("'O' won! Game over!");
+         } else if (currentState == GameState.DRAW) {
+            System.out.println("It's a draw! Game over!");
+         }
+		 
+         currentPlayer = (currentPlayer == Seed.CROSS) ? Seed.NOUGHT : Seed.CROSS;
+      } while (currentState == GameState.PLAYING);
+   }
+
+   public void initGame() {
+      board.init();
+      currentPlayer = Seed.NONE;       // None plays first
+      currentState = GameState.PLAYING;
+   }
+
+   public void playerMove(Seed theSeed) {
+      boolean validInput = false;
+      do {
+         if (theSeed == Seed.NONE) {
+            System.out.print("Player 'O', enter your move (Row[1-3] and Column[1-3]): ");
+         } else {
+            System.out.print("Player 'X', enter your move (Row[1-3] and Column[1-3]): ");
+         }
+         int row = in.nextInt() - 1;
+         int col = in.nextInt() - 1;
+         if (row >= 0 && row < Board.ROWS && col >= 0 && col < Board.COLS
+               && board.cells[row][col].content == Seed.EMPTY) {
+            board.cells[row][col].content = theSeed;
+            board.currentRow = row;
+            board.currentCol = col;
+            validInput = true;
+         } else {
+            System.out.println("This move at (" + (row + 1) + "," + (col + 1)
+                  + ") is not valid. Try again.");
+         }
+      } while (!validInput);
+   }
+
+   public void updateGame(Seed theSeed) {
+      if (board.hasWon(theSeed)) {
+         currentState = (theSeed == Seed.CROSS) ? GameState.CROSS_WON : GameState.NOUGHT_WON;
+      } else if (board.isDraw()) {
+         currentState = GameState.DRAW;
+      }
+   }
+
+   public static void main(String[] args) {
+      new GameMain();  // Bahala na constructor gumawa dito
+   }
 }
