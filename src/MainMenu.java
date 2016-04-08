2814e28f0e88ea1c09f8742a2df663b4cfaf0ca8
amsartillo <amsartillo1@up.edu.ph>
2016-04-08 23:30:57 +0800

    Docu for MainMenu

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -4,34 +4,34 @@ import java.awt.event.*;
 
 public class MainMenu extends JPanel{
 	
-	JPanel[] panels = new JPanel[4];
-	JTextField[] textField = new JTextField[3];	
+	JPanel[] panels = new JPanel[4];								//sets new JPanels
+	JTextField[] textField = new JTextField[3];						//sets text field
 	JButton ok;
 	JButton cancel;	
 	
 	public MainMenu() {
 		super(new FlowLayout());
 
-		for(int i=0;i<4;i++){
+		for(int i=0;i<4;i++){										//for loop for setting all jpanels
 			panels[i] = new JPanel(new FlowLayout());
 			panels[i].setPreferredSize(new Dimension(300, 50));
 			this.add(panels[i]);
 		}	
 			
-		JLabel label1 = new JLabel("Player 1 name: ");
+		JLabel label1 = new JLabel("Player 1 name: ");				//sets both player names
 		JLabel label2 = new JLabel("Player 2 name: ");
 		JLabel label3 = new JLabel("Best of: ");
 
-		panels[0].add(label1);
+		panels[0].add(label1);										//gives the JLabels w/ set names 
 		panels[1].add(label2);
 		panels[2].add(label3);
 	
-		for(int i = 0;i<3;i++){
+		for(int i = 0;i<3;i++){										//sets text field
 			textField[i] = new JTextField(10);
 			panels[i].add(textField[i]);
 		}		
 	
-		ok = new JButton("OK");
+		ok = new JButton("OK");										//sets ok and cancel buttons in frame
 		cancel = new JButton("CANCEL");
 		panels[3].add(ok);
 		panels[3].add(cancel);
@@ -42,6 +42,8 @@ public class MainMenu extends JPanel{
 		});
 	}
 
+	//GETTERS
+	
 	public JButton getOKButton(){
 		return ok;
 	}
